b123d8077cb5be5a0d4e771fce488a51e3504d89
Beepboop <anthonyjventarola@gmail.com>
2025-04-28 12:12:39 -0400

    Create multistep_phrases_to_delete.py

---
+++ multistep_phrases_to_delete.py
@@ -0,0 +1,19 @@
+multistep_phrases_to_delete = [
+    # Feed me your multiline strings.
+
+    # mshta
+    {
+        r"parentImage\":\"C:\\\\\\\\Windows\\\\\\\\SysWOW64\\\\\\\\mshta.exe\",\"parentCommandLine\":\"\\\\\\\"C:\\\\\\\\WINDOWS\\\\\\\\SysWOW64\\\\\\\\mshta.exe\\\\\\\" \\\\\\\"C:\\\\\\\\Program Files\\\\\\\\BLABLABLA\\\\\\\\BLABLABLA\\\\\\\\Bla\\\\\\\\BLA.hta",
+        r"Employer_Specific_Endpoint_Hostname_Here"
+    },
+
+    {
+        r"parentImage\":\"C:\\\\\\\\Windows\\\\\\\\SysWOW64\\\\\\\\mshta.exe\",\"parentCommandLine\":\"\\\\\\\"C:\\\\\\\\WINDOWS\\\\\\\\SysWOW64\\\\\\\\mshta.exe\\\\\\\" \\\\\\\"C:\\\\\\\\Program Files\\\\\\\\BLABLABLA\\\\\\\\BLABLABLA\\\\\\\\Bla\\\\\\\\BLA.hta",
+        r"Employer_Specific_Endpoint_Hostname_Here",
+        r""
+
+    # et cetera Keep going!
+    },
+
+
+]
